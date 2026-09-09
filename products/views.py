54f7b2086f1f41c3676629c3287454bd500650f0
Sarv <immortalcypress@gmail.com>
2026-09-09 09:00:39 -0700

    products/views ,is_availabel changed to is_active previous one was wrong

--- products/views.py
+++ products/views.py
@@ -5,11 +5,11 @@ from .models import Product
 def home(request):
     products = (
         Product.objects
-        .filter(is_available=True)
-        .select_related('store')
-        .order_by('-created_at')
+        .filter(is_active=True)
+        .select_related("store")
+        .order_by("-created_at")
     )
-    
+
     return render(
         request,
         "products/home.html",
