36509b39d802d602d241515c54945b9b9778cb0f
Sarv <immortalcypress@gmail.com>
2026-09-10 07:21:49 -0700

    store/view.py   store list created

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Store
+
+
+def store_list(request):
+    stores = (
+        Store.objects
+        .filter(is_active=True)
+        .order_by("name")
+    )
+
+    return render(
+        request,
+        "store/store_list.html",
+        {"stores": stores},
+    )
