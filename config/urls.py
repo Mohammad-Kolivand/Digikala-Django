8e2c95316c398b1e27b612e7bad9cf3b5c0a3404
Sarv <immortalcypress@gmail.com>
2026-09-11 06:43:40 -0700

    login path added

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from products.views import home
+from accounts.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('login/', login_view, name='login'),
     path("stores/", include("store.urls")),
 ]
