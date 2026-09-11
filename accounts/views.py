2bbebe410825a62c827e186bf7b582ba89bbc4db
Sarv <immortalcypress@gmail.com>
2026-09-11 10:41:03 -0700

    signup form created in view.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+from .models import CustomerProfile, SellerProfile
 
 
 def login_view(request):
@@ -27,3 +29,33 @@ def login_view(request):
         )
 
     return render(request, "accounts/login.html")
+
+
+def signup_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        role = request.POST.get("role")
+
+        if form.is_valid():
+            if role not in {"customer", "seller"}:
+                form.add_error(
+                    None,
+                    "لطفاً نوع حساب را انتخاب کنید.",
+                )
+            else:
+                user = form.save()
+
+                if role == "customer":
+                    CustomerProfile.objects.create(user=user)
+                else:
+                    SellerProfile.objects.create(user=user)
+
+                return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(
+        request,
+        "accounts/signup.html",
+        {"form": form},
+    )
